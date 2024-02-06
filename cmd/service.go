edbbf18e503f579f5d0f73232a09a84625923379
Christian <cb@beeltec.com>
2024-02-06 15:32:15 +0100

    Fixed display of OS notice being shown every time

--- cmd/service.go
+++ cmd/service.go
@@ -3,23 +3,19 @@
 package cmd
 
 import (
-	"os"
-	"runtime"
-
 	"github.com/fatih/color"
 	"github.com/pocketbase/pocketbase/core"
 	"github.com/spf13/cobra"
 )
 
 func NewServiceCommand(app core.App) *cobra.Command {
-	if runtime.GOOS != "windows" {
-		color.Yellow("This command can only run on Windows")
-		os.Exit(1)
-	}
-
 	command := &cobra.Command{
 		Use:   "service",
 		Short: "Manages Windows service registration",
+		RunE: func(command *cobra.Command, args []string) error {
+			color.Yellow("This command can only be run on Windows")
+			return nil
+		},
 	}
 
 	return command
